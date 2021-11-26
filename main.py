f2a6e5229b70662144cf65845c54404cc9a4bcf6
kaajjaak <yolobroer202@gmail.com>
2021-11-26 16:24:54 +0100

    added status

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 
 import uvicorn
 from cryptography.fernet import Fernet
-from fastapi import FastAPI
+from fastapi import FastAPI, Response, status
 import sqlite3
 import jwt
 from pydantic import BaseModel
@@ -11,7 +11,7 @@ SECRET = "b8e32c1e0a8c6af7b04b1fe193c4293e1c4af76e1456a683"
 cipher_suite = Fernet(b"viGCeC-_tdTJxDb72yWIzFkI4VO5H-fIE9btMX6iTGE=")
 
 
-class Item(BaseModel):
+class Account(BaseModel):
     username: str
     password: str
 
@@ -29,33 +29,34 @@ async def root():
 
 
 @app.post("/accounts")
-async def create_item(item: Item):
-    item_dict = item.dict()
+async def create_account(account: Account, response: Response):
+    account_dict = account.dict()
     conn = sqlite3.connect("accounts.db")
     cur = conn.cursor()
     sql = "INSERT INTO accounts(username, password, token) VALUES(?, ?, ?)"
-    encoded_jwt = jwt.encode({"username": item_dict["username"], "password": item_dict["password"]}, SECRET,
+    encoded_jwt = jwt.encode({"username": account_dict["username"], "password": account_dict["password"]}, SECRET,
                              algorithm="HS256")
-    params = [item_dict["username"], cipher_suite.encrypt(str.encode(item_dict["password"])), encoded_jwt]
+    params = [account_dict["username"], cipher_suite.encrypt(str.encode(account_dict["password"])), encoded_jwt]
     cur.execute(sql, params)
     conn.commit()
     conn.close()
-    return 200
+    response.status_code = status.HTTP_201_CREATED
+    return account_dict["username"]
 
 
 @app.get("/accounts")
-async def get_item(item: Item):
-    item_dict = item.dict()
+async def get_item(account: Account):
+    account_dict = account.dict()
     conn = sqlite3.connect("accounts.db")
     cur = conn.cursor()
     sql = "SELECT password FROM accounts WHERE username = ?"
-    cur.execute(sql, [item_dict["username"]])
+    cur.execute(sql, [account_dict["username"]])
     for row in cur.fetchall():
         password = row[0]
         break
-    if cipher_suite.decrypt(password) == str.encode(item_dict["password"]):
+    if cipher_suite.decrypt(password) == str.encode(account_dict["password"]):
         sql = "SELECT token FROM accounts WHERE username = ?"
-        cur.execute(sql, [item_dict["username"]])
+        cur.execute(sql, [account_dict["username"]])
         for row in cur.fetchall():
             token = row[0]
             break
