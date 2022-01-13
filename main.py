74c832b172c6c10c83d9c5884bc8ecd6da9feadf
kaajjaak <yolobroer202@gmail.com>
2022-01-13 20:35:06 +0100

    fixed one error hopefully try 2

--- main.py
+++ main.py
@@ -89,7 +89,7 @@ async def get_homepage(token: Token):
     cur = conn.cursor()
     sql = "SELECT username FROM accounts WHERE token = ?"
     cur.execute(sql, [token_dict["token"]])
-    username = cur.fetchone()
+    username = cur.fetchall()[0][0]
     conn.close()
     return {"username": username}
 
