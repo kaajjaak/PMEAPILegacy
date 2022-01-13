44a2c538c973b53dd2c0e4e6be8c0ad2632dca26
kaajjaak <yolobroer202@gmail.com>
2022-01-13 22:38:11 +0100

    debuging sadge

--- main.py
+++ main.py
@@ -105,16 +105,12 @@ async def change_password(newAccount: NewAccount):
     cur = conn.cursor()
     sql = "SELECT password FROM accounts where token=?"
     cur.execute(sql, [account_dict["token"]])
-    pass_dict = {}
-    for row in cur.fetchall():
-        pass_dict["password"] = row[0]
-        break
-    print(pass_dict["password"])
-    if cipher_suite.decrypt(pass_dict["password"]) != account_dict["current_password"]:
-        return {"status": 'nope'}
-    sql = "UPDATE accounts SET password = ? WHERE token = ?"
-    print(cipher_suite.encrypt(str.encode(account_dict["new_password"])))
-    cur.execute(sql, [cipher_suite.encrypt(str.encode(account_dict["new_password"])), account_dict["token"]])
+    print(cur.fetchone())
+    # if cipher_suite.decrypt(pass_dict["password"]) != account_dict["current_password"]:
+    #     return {"status": 'nope'}
+    # sql = "UPDATE accounts SET password = ? WHERE token = ?"
+    # print(cipher_suite.encrypt(str.encode(account_dict["new_password"])))
+    # cur.execute(sql, [cipher_suite.encrypt(str.encode(account_dict["new_password"])), account_dict["token"]])
     conn.close()
     return {"status": "success"}
 
