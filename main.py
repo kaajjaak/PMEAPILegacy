3507fe78509678b2197e2041b94e40c555ae69fd
kaajjaak <46300531+kaajjaak@users.noreply.github.com>
2022-02-02 09:01:53 +0000

    maybe fix copium

--- main.py
+++ main.py
@@ -147,7 +147,8 @@ async def list_applications(token: Token, response: Response):
     conn = sqlite3.connect("accounts.db")
     cur = conn.cursor()
     sql = "SELECT applicationID, name FROM application WHERE applicationID in (SELECT * FROM AccountApplicationConnection WHERE IDAccount in (SELECT id FROM accounts WHERE token = ?))"
-    applications = cur.execute(sql, [token_dict["token"]]).fetchall()
+    cur.execute(sql, (token_dict["token"],))
+    applications = cur.fetchall()
     response.status_code = status.HTTP_202_ACCEPTED
     conn.close()
     return applications
