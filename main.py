1f29bfb3877c48da2d505b5bdbff22c6567d443e
kaajjaak <46300531+kaajjaak@users.noreply.github.com>
2022-02-02 08:49:52 +0000

    I don't think I need the fetchall

--- main.py
+++ main.py
@@ -147,7 +147,7 @@ async def list_applications(token: Token, response: Response):
     conn = sqlite3.connect("accounts.db")
     cur = conn.cursor()
     sql = "SELECT * FROM application WHERE applicationID in (SELECT * FROM AccountApplicationConnection WHERE IDAccount in (SELECT id FROM accounts WHERE token = ?))"
-    applications = cur.execute(sql, [token_dict["token"]]).fetchall()
+    applications = cur.execute(sql, [token_dict["token"]])
     response.status_code = status.HTTP_202_ACCEPTED
     conn.close()
     return applications
