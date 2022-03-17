b332ad90e1cad2268fd51c59d275cb727534f065
kaajjaak <yolobroer202@gmail.com>
2022-03-17 17:37:34 +0100

    hmm

--- main.py
+++ main.py
@@ -161,12 +161,13 @@ async def list_applications(token: Token, response: Response):
 
 @app.post("/application/{app_id}/process/createProcess")
 async def add_process(app_id: int, process: Process, response: Response):
-    app_id = {"app_id": app_id}
+    appl_id = {"app_id": app_id}
+    print(appl_id)
     process_dict = process.dict()
     conn = sqlite3.connect("accounts.db")
     cur = conn.cursor()
     sql = "SELECT token FROM accounts WHERE id IN (SELECT IDAccount FROM AccountApplicationConnection WHERE IDApplication = ?)"
-    cur.execute(sql, [app_id])
+    cur.execute(sql, [appl_id])
     found = False
     for row in cur.fetchall():
         if row[0] == process_dict["jwt"]:
