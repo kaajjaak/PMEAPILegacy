0906b7ce0494344da8ab26c487b334d837fe4302
kaajjaak <yolobroer202@gmail.com>
2022-05-10 21:41:27 +0200

    make it better

--- testrequests.py
+++ testrequests.py
@@ -1,9 +1,12 @@
 import requests
 
-account = {"username": "test", "password": "kaas"}
-r = requests.get("https://databasegip.herokuapp.com/accounts", json=account)
-token = r.json()
-print(token)
+# account = {"username": "test", "password": "kaas"}
+# r = requests.get("https://databasegip.herokuapp.com/accounts", json=account)
+# token = r.json()
+# print(token)
 # r = requests.get("http://localhost:8000/pages/homepage", json={"token": token})
 # print(r.text)
 
+account = {"username": "banaan", "password": "peer"}
+r = requests.post("http://127.0.0.1:8000/accounts", json=account)
+print(r)
